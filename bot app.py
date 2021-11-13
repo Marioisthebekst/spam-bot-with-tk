34a718bdcdc70c913bc48f2a5d2946734b24ae30
Marioisthebekst <80685718+Marioisthebekst@users.noreply.github.com>
2021-11-13 12:57:10 +0200

    Update bot app.py

--- bot app.py
+++ bot app.py
@@ -1,19 +1,24 @@
+# the program may will crash while you spamming *
+# importing
 from tkinter import *
 import pyautogui, time
 
+# the spam command
 def spamming():
+
     for i in range(int(amount.get())):
         time.sleep(0.3)
         pyautogui.write(text.get())
         pyautogui.press('enter')
 
+# making the window
 WIN = Tk()
 WIN.geometry("500x300")
 WIN.title("Spam Bot")
 
+# making the texts and the outputs
 lbl = Label(WIN, text="type what you want to spam")
 lbl.pack()
-
 text = Entry(WIN)
 text.pack()
 
@@ -23,7 +28,10 @@ lbl_amount.pack()
 amount = Entry(WIN)
 amount.pack()
 
-btn = Button(WIN, text="Spam!", bg="#cce3e6", fg="black", command= spamming)
+# the spam button
+btn = Button(WIN, text="SPAM", bg="#cce3e6", fg="black", command= spamming)
 btn.pack()
 
+
+# this is for stay the window on until i close
 WIN.mainloop()
